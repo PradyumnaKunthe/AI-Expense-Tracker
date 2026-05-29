5c4376be50229929686de07e27e3432fcf9f3352
PradyumnaKunthe <Kunthepraduman@gmail.com>
2026-05-29 22:47:05 +0530

    Fix Render port binding

--- app.py
+++ app.py
@@ -428,4 +428,11 @@ def charts_page():
 # ─────────────────────────────────────────
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    init_db()
+
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port
+    )
